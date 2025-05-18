f6180e9972d7e943334b6b4fb2e853e9c70c1578
Miguel Balbo Victor <miguel.balbo@yahoo.com.br>
2025-05-18 17:34:02 -0300

    Create Inscricao.java

---
+++ src/model/Inscricao.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Inscricao {
+	private int codProcesso;
+	
+	public Inscricao(int codProcesso) {
+		this.codProcesso = codProcesso;
+	}
+
+	public int getCodProcesso() {
+		return codProcesso;
+	}
+
+	public void setCodProcesso(int codProcesso) {
+		this.codProcesso = codProcesso;
+	}
+	
+	
+}
